ad40597e208f72b97af1cf0e8ebf8dcabf76e7c1
AlexAgredo <48070011+AlexAgredo@users.noreply.github.com>
2022-03-16 19:53:06 -0500

    ejercicio javascript

---
+++ Semana7_introJS/getElement.js
@@ -0,0 +1,6 @@
+
+document.getElementById("p").innerHTML = "MJ"
+document.getElementsByClassName("titulo")[0].innerHTML = "Les presento a Michael Jordan" //como dice elements es un arreglo ya que trae todas las clases 
+document.getElementsByTagName("h2")[0].innerHTML = "No perdon, michael jackson"
+document.getElementsByTagName("img")[0].src = "https://as01.epimg.net/ascolor/imagenes/2020/04/24/opinion/1587724098_688387_1587724809_noticia_normal.jpg"
+// document.getElementsByTagName("h2").innerHTML = "No te dejes confundir"
